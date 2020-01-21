a91306ba8174bb9ae1ad981aeee8258a12caa341
Pablo Pelayo <pablo.pelayo@gmail.com>
2020-01-21 01:23:34 -0300

    update

--- inicial/practico.py
+++ inicial/practico.py
@@ -7,8 +7,7 @@ from tkinter import messagebox
 
 
 # Crear base de datos
-
-def crearbd():
+def f_creardb():
     try:
         mibase = mysql.connector.connect(host="localhost", user="root", passwd="")
         micursor = mibase.cursor()
@@ -20,12 +19,13 @@ def crearbd():
     except:
         print("Ya existe la base de datos con su tabla")
 
-
+#Conexion
 def miconexion():
     mibase = mysql.connector.connect(host="localhost", user="root", passwd="", database="gastos_casa")
     return mibase
 
-def alta():
+#Agregar
+def f_agregar():
     print("Nueva alta de datos")
 
     cadena = nombre.get()
@@ -41,6 +41,17 @@ def alta():
     else:
        print("NO validado")        
     
+#Refresh
+def f_refresh():
+    tree.delete(*tree.get_children())
+    mibase = miconexion()
+    micursor = mibase.cursor()
+    sql = "SELECT * FROM gastos"
+    micursor.execute(sql)
+    resultado = micursor.fetchall()
+
+    for x in resultado:
+        tree.insert("", "end", values=x)
 
 
 # Defino la ventana principal de la aplicación
@@ -65,19 +76,28 @@ e_text1 = ttk.Entry(root, textvariable=nombre, width=15)
 e_text2 = ttk.Entry(root, textvariable=evento, width=15)
 e_text3 = ttk.Entry(root, textvariable=gasto, width=15)
 
-b_agregar = ttk.Button(root, text='Agregar', command=alta)
-b_refresh = ttk.Button(root, text='Refresh')#, command=fborrar)
+b_agregar = ttk.Button(root, text='Agregar', command=f_agregar)
+b_refresh = ttk.Button(root, text='Refresh', command=f_refresh)
 b_salir = ttk.Button(root, text='Salir', command=root.quit)
-b_creardb = ttk.Button(root, text='Crear DB', command= crearbd)
+b_creardb = ttk.Button(root, text='Crear DB', command=f_creardb)
 
+#Treeview
 tree = ttk.Treeview(root)
-tree["columns"] = ("col1")
-tree.column("#0", width=0, minwidth=0, stretch=NO)
-tree.column("col1", width=400, minwidth=100, stretch=NO)
-tree.heading("col1", text="Tabla")
+tree["columns"] = ("col1", "col2", "col3", "col4")
+tree.column("#0", width=0, minwidth=0)
+tree.column("col1", width=50, minwidth=50)
+tree.column("col2", width=100, minwidth=100)
+tree.column("col3", width=100, minwidth=100)
+tree.column("col4", width=100, minwidth=100)
+
+tree.heading("col1", text="ID")
+tree.heading("col2", text="Persona")
+tree.heading("col3", text="Evento")
+tree.heading("col4", text="Gasto")
+
 style = ttk.Style()
-style.configure(".", font=('Tahoma', 10), foreground="black")
-style.configure("Treeview.Heading", foreground='red')
+style.configure(".", font=('Tahoma', 10), foreground="royal blue")
+style.configure("Treeview.Heading", foreground='maroon3')
 
 b_agregar.grid(column=4, row=0)
 b_refresh.grid(column=4, row=1)
